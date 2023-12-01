7df6d0d41985154bcb321774550ec72d23d298d9
Светлана <studywork304@gmail.com>
2023-12-01 16:28:56 +0700

    прописано окно для добавления картинки из файла

---
+++ MusPicCatalog/MusPicCatalog/add_comp.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MusPicCatalog
+{
+    /// <summary>
+    /// Логика взаимодействия для add_comp.xaml
+    /// </summary>
+    public partial class add_comp : Window
+    {
+        public add_comp()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
